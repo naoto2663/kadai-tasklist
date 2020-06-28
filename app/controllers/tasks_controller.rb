1157759a0c6e6f8c328ffc250ab092e4f4f1e443
naoto.tsuchiya <football.b63.vno.1@gmail.com>
2020-06-28 11:15:38 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -24,7 +25,6 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
@@ -40,17 +40,21 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
 
         flash[:success] = 'タスクは正常に削除されました'
         redirect_to tasks_url
     end
-end
 
-private
+    private
 
-  # Strong Parameter
-def task_params
-    params.require(:task).permit(:content)
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+
+    def task_params
+        params.require(:task).permit(:content)
+    end
+
 end
